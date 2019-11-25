3e491c0e329b7f0f9e6dedd65836a735b896842a
alshaboti <alshaboti.it@gmail.com>
2019-11-25 14:55:36 +1300

    Okay for single device single function type

--- cp-sat1.py
+++ cp-sat1.py
@@ -7,21 +7,9 @@ from __future__ import print_function
 from ortools.sat.python import cp_model
 import itertools
 import numpy as np 
+import time
 
 #developers.google.com/optimization/cp/cp_solver
-#def perm(vec):
-#    indx = [i for i,v in enumerate(vec) if v =='1']
-#    prm = ["".join(seq) for seq in itertools.product("01",repeat=len(indx))]
-#    prm_num = len(prm)
-#    vec_len = len(vec)
-#    prm_z_v =['0'*vec_len for i in range(prm_num)]
-#    for prm_i in range(prm_num):
-#        temp = list(prm_z_v[prm_i])
-#        for j,bit_loc in enumerate(indx):
-#            temp [bit_loc] = list(prm[prm_i])[j]
-#            prm_z_v[prm_i]=int("".join(temp),2)
-#
-#    return prm_z_v 
 
 def NotSimpleSatProgram():
     """Minimal CP-SAT example to showcase calling the solver."""
@@ -36,34 +24,42 @@ def NotSimpleSatProgram():
                           [0,1,1,0],
                           [1,0,1,1]])
 
+    w = np.array( [[1,0,1,1], 
+                   [0,1,1,1]])
+
     # to save the index of functions in the flat devicces_var list                      
-    func_wise_var = [ [] for f in range(function_num)] 
+    dev_per_fun = [ [] for f in range(function_num)] 
     devices_var = []
     for d in range(devices_num):
         for f in range(function_num):
             if device_cap[d][f]:
-                devices_var.append(model.NewIntVar(0,1,'x_'+str(d)+str(f)))
-                func_wise_var[f].append( len(devices_var)-1 )
-    print(func_wise_var)                
-    
+                var_d = model.NewIntVar(0,device_cap[d][f],'x_'+str(d)+str(f))
+                devices_var.append(var_d)
+                dev_per_fun[f].append( len(devices_var)-1 )
+
+
     # Creates the constraints.
         # 1- binary constraints, don't use x00 with x13 
     model.Add(devices_var[1]+devices_var[8]<=1)
         # 2- triple constraints, don't use x1 with x2, if x3 is used (only work with binary var)
-    model.Add(devices_var[5]+devices_var[8]<=1).OnlyEnforceIf(devices_var[0])
+    model.Add(devices_var[5]+devices_var[7]<=1).OnlyEnforceIf( devices_var[0] )
 
-    w = np.array( [[1,0,1,1], 
-                   [0,1,1,1]])
-
+ 
+#   for wi in w:
+#        # 1- binary constraints, don't use x00 with x13 
+#        model.Add(wi[2]*devices_var[1]+wi[3]*devices_var[8]<=1)
+#        # 2- triple constraints, don't use x1 with x2, if x3 is used (only work with binary var)
+#        if wi[0]:
+#            model.Add(wi[2]*devices_var[5]+wi[2]*devices_var[7]<=1).OnlyEnforceIf( devices_var[0]*wi[0] )
+#
     # constraint to select the minimum number of devices that are enough to satisfy the workflows
         # total functions req in the workflow
     wf = list(w.sum(axis=0))
     for idx_f,num_f in enumerate(wf):    
-        model.Add( sum( [devices_var[i] for i in func_wise_var[idx_f] ] ) +1 > num_f)
-        model.Add( sum( [devices_var[i] for i in func_wise_var[idx_f] ] ) <= num_f)
+        model.Add( sum( [devices_var[i] for i in dev_per_fun[idx_f] ] ) +1 > num_f)
 
    #optimization 
-        # secure score
+         # secure score
     icvss=[3,3,1,1,1,1,1,1,1]
     assert len(icvss)==len(devices_var), "CVSS should be for all decision variables"
 
@@ -71,9 +67,10 @@ def NotSimpleSatProgram():
 
     # Creates a solver and solves the model.
     solver = cp_model.CpSolver()
-
+    start = time.time()
     status = solver.Solve(model)
-
+    end = time.time()
+    print("Elapsed time: ", end-start)
     if status == cp_model.OPTIMAL: # cp_model.FEASIBLE:
         print(w)
         for v in devices_var:
@@ -84,6 +81,3 @@ def NotSimpleSatProgram():
 
 NotSimpleSatProgram()    
 
-# limitations
-# 1- each device can only used once for each function at a time. Still it can perform multiple different functions
-# 2- The model will produce partial solution if no feasible solution due to the minimum devices selection constraint. 
